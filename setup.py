6bec498574fd57bf60ab62fa331e84ee920f8e48
Joe Stein <joeaarons@gmail.com>
2021-04-04 09:10:03 -0400

    Add info to setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,11 @@ from distutils.core import setup, Extension
 setup(
     name='pybeep',
     version='0.1',
-    ext_modules=[Extension("pybeep", ["_pybeep.c", "pybeep.c"])]
+    ext_modules=[Extension("pybeep", ["_pybeep.c", "pybeep.c"])],
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux"
+    ],
+    url="https://github.com/jas14/pybeep",
 )
